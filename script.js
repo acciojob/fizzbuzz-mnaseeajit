d1bc4367f79547f6ca95b7f1a8ccc7ffe9580c70
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-08 13:34:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let ans = "";
+   for(let i = 1;i<=20;i++)
+	   {
+		   if(i%3 == 0){
+			   ans += "Fizz "+ "\n";
+		   }
+		   else if(i%5 == 0){   
+			   ans += "Buzz "+ "\n";
+		   }
+		   else if(i%3 == 0 && i%5==0){
+			   ans += 'FizzBuzz '+ "\n";
+		   }
+		   else{
+			   ans += i+" "+ "\n";
+		   }
+	   }
+  alert(ans);
